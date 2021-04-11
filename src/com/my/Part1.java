fab59c61a57b3f649cee3e77b63b72b3a8a36ec0
Andrii Cherniak <ia87@ukr.net>
2021-04-11 18:48:24 +0300

    Exrtract tasks 1-3 into the methods + refactiring

--- src/com/my/Part1.java
+++ src/com/my/Part1.java
@@ -2,32 +2,47 @@ package com.my;
 
 import java.math.BigDecimal;
 import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Part1 {
-
-    private static int counter = 0;
-
     public static void main(String[] args) {
-        String task1 = "Take all numbers from stream until ‘target’ value is met, then calculate sum.";
-        List<Integer> integers = Arrays.asList(4, 44, 2, 44, 9, 10, 1, 7);
-        List<BigDecimal> bigDecimalList = integers.stream().map(BigDecimal::valueOf).collect(Collectors.toList());
-        BigDecimal target = new BigDecimal(9);
+        task1();
+        task2();
+        task3();
+    }
 
-        BigDecimal sum = bigDecimalList.stream().limit(bigDecimalList.indexOf(target)).reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
-        System.out.printf("Task#1: %s\nInput list: %s\nTarget number: %s\nSum: %s\n\n", task1, bigDecimalList, target, sum);
+    private static void task1() {
+        final String TASK_1 = "Take all numbers from stream until ‘target’ value is met, then calculate sum.";
+        final List<Integer> integers = Arrays.asList(4, 44, 2, 44, 9, 10, 1, 7);
+        final List<BigDecimal> bigDecimalList = integers.stream().map(BigDecimal::valueOf).collect(Collectors.toList());
+        final BigDecimal target = new BigDecimal(9);
 
-        String task2 = "Given List of Strings, return a List of String according to condition:\n" +
-                        "each first String should have prefix “pre”, each second String should have prefix “post”.";
-        List<String> stringList1 = Arrays.asList("production", "analysis", "condition");
-        List<String> stringList2 = stringList1.stream().map(s -> {
-            counter++;
-            return counter % 2 == 1 ? "pre" + s : "post" + s;
-        }).collect(Collectors.toList());
-        System.out.printf("Task#2: %s\nInput list: %s\nOutputList: %s\n\n", task2, stringList1, stringList2);
+        BigDecimal sum = bigDecimalList.stream()
+                .takeWhile(Predicate.isEqual(target))
+                .reduce(BigDecimal::add)
+                .orElse(BigDecimal.ZERO);
+        System.out.printf("Task#1: %s\nInput list: %s\nTarget number: %s\nSum: %s\n\n", TASK_1, bigDecimalList, target, sum);
+    }
 
-        String task3 = "Given List of Strings, count occurrences of each string and sort by the number of occurrences, return first 3 most popular strings.";
-        List<String> brands = Arrays.asList("audi", "bmw", "chevrolet", "audi", "volkswagen", "mazda", "kia", "volkswagen", "audi", "bmw");
+    private static void task2() {
+        final String TASK_2 = "Given List of Strings, return a List of String according to condition:\n" +
+                "each first String should have prefix “pre”, each second String should have prefix “post”.";
+        final List<String> stringList1 = Arrays.asList("production", "analysis", "condition");
+        final String PREFIX_PRI = "pre";
+        final String PREFIX_POST = "post";
+
+        List<String> stringList2 = IntStream
+                .range(0, stringList1.size())
+                .mapToObj(i -> i % 2 == 1 ? PREFIX_PRI + stringList1.get(i) : PREFIX_POST + stringList1.get(i))
+                .collect(Collectors.toList());
+        System.out.printf("Task#2: %s\nInput list: %s\nOutputList: %s\n\n", TASK_2, stringList1, stringList2);
+    }
+
+    private static void task3() {
+        final String TASK_3 = "Given List of Strings, count occurrences of each string and sort by the number of occurrences, return first 3 most popular strings.";
+        final List<String> brands = Arrays.asList("audi", "bmw", "chevrolet", "audi", "volkswagen", "mazda", "kia", "volkswagen", "audi", "bmw");
 
         List<String> brandsLimited = brands.stream()
                 //  can be replaced by:
@@ -39,9 +54,7 @@ public class Part1 {
                         .compareTo(o2.getValue()))
                 .limit(3).map(Map.Entry::getKey)
                 .collect(Collectors.toList());
-        System.out.printf("Task#3: %s\nInput list: %s\nOutputList: %s\n\n", task3, brands, brandsLimited);
-
-
-
+        System.out.printf("Task#3: %s\nInput list: %s\nOutputList: %s\n\n", TASK_3, brands, brandsLimited);
     }
+
 }
